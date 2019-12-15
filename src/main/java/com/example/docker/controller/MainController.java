cf5eb5288f8fda6d2e062fe6738c2b3af6346592
younghokwak <jason.uphiller@gmail.com>
2019-12-15 15:11:37 +0900

    api 추가

--- src/main/java/com/example/docker/controller/MainController.java
+++ src/main/java/com/example/docker/controller/MainController.java
@@ -5,15 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class MainController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/")
+    @GetMapping("/user")
     public ResponseEntity<?> getBuoy(){
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
--- src/test/java/com/example/docker/DockerApplicationTests.java
+++ src/test/java/com/example/docker/DockerApplicationTests.java
@@ -1,13 +1,67 @@
 package com.example.docker;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DockerApplicationTests {
+public class DockerApplicationTests {
+
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mockMvc;
+	private RestDocumentationResultHandler document;
+
+	@Autowired
+	ObjectMapper mapper;
+
+	@Before
+	public void setUp() {
+		this.document = document(
+				"{class-name}/{method-name}",
+				preprocessResponse(prettyPrint())
+		);
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.addFilters(new CharacterEncodingFilter("UTF-8", true))
+				.apply(documentationConfiguration(this.restDocumentation))
+				.alwaysDo(document)
+				.build();
+	}
 
 	@Test
-	void contextLoads() {
+	public void 회원리스트() throws Exception {
+
+		mockMvc.perform(
+				get("/v1/user")
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
+
 
 }
